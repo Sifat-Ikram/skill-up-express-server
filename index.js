0a167c2e614c577ae5e537032d9e03a3f77973ca
Sifat Ikram <sifatikram@gmail.com>
2024-03-07 21:22:51 +0600

    instructor part added

--- index.js
+++ index.js
@@ -8,10 +8,7 @@ const port = process.env.PORT || 4321;
 app.use(express.json());
 app.use(cors());
 
-
-
-
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jrqljyn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -20,7 +17,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -31,36 +28,65 @@ async function run() {
     const courseCollection = client.db("skillExpress").collection("course");
     const categoryCollection = client.db("skillExpress").collection("category");
     const cartCollection = client.db("skillExpress").collection("cart");
+    const reviewCollection = client.db("skillExpress").collection("review");
+    const instructorCollection = client.db("skillExpress").collection("instructor");
 
     // course api
     app.get("/course", async (req, res) => {
-        const result = await courseCollection.find().toArray();
-        res.send(result);
+      const result = await courseCollection.find().toArray();
+      res.send(result);
     });
 
     app.post("/course", async (req, res) => {
-        const course = req.body;
-        const result = await courseCollection.insertOne(course);
-        res.send(result);
-    })
+      const course = req.body;
+      const result = await courseCollection.insertOne(course);
+      res.send(result);
+    });
     app.delete("/course/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await courseCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    app.patch("/course/:id", async (req, res) => {
+      try {
         const id = req.params.id;
-        const query = { _id: new ObjectId(id) };
-        const result = await courseCollection.deleteOne(query);
-        res.send(result);
-    })
+        const filter = { _id: new ObjectId(id) };
+        const updateFields = req.body;
+
+        const result = await courseCollection.updateOne(filter, {
+          $set: updateFields,
+        });
+
+        if (result.modifiedCount === 1) {
+          res.status(200).json({ message: "Course updated successfully" });
+        } else {
+          res.status(404).json({ message: "Course not found" });
+        }
+      } catch (error) {
+        console.error("Error updating course:", error);
+        res.status(500).json({ message: "Internal server error" });
+      }
+    });
 
     // category api
     app.get("/category", async (req, res) => {
-        const result = await categoryCollection.find().toArray();
-        res.send(result);
+      const result = await categoryCollection.find().toArray();
+      res.send(result);
+    });
+
+    // instructor api
+    app.get("/instructor", async (req, res) => {
+      const result = await instructorCollection.find().toArray();
+      res.send(result);
     });
 
     // cart api
     app.post("/cart", async (req, res) => {
-        const course = req.body;
-        const result = await cartCollection.insertOne(course);
-        res.send(result);
+      const course = req.body;
+      const result = await cartCollection.insertOne(course);
+      res.send(result);
     });
 
     app.get("/cart", async (req, res) => {
@@ -77,15 +103,23 @@ async function run() {
       res.send(result);
     });
 
+    // review api
+    app.post("/review", async (req, res) => {
+      const course = req.body;
+      const result = await reviewCollection.insertOne(course);
+      res.send(result);
+    });
 
-
-
-
-
+    app.get("/review", async (req, res) => {
+      const result = await reviewCollection.find().toArray();
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -93,13 +127,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-
-
-app.get('/', (req, res) => {
-    res.send("skillup express is running")
-})
+app.get("/", (req, res) => {
+  res.send("skillup express is running");
+});
 
 app.listen(port, () => {
-    console.log(`skillup express is running through port ${port}`);
-})
+  console.log(`skillup express is running through port ${port}`);
+});
